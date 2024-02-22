ca6f5db6f67db4fe3c7f4f296dbc479f7af3f9c0
s0kil <dtechtours@gmail.com>
2024-02-22 10:23:41 -0500

    MPLabX PIC Memory View

--- S19pro-PIC-disassemble.sh
+++ S19pro-PIC-disassemble.sh
@@ -7,3 +7,6 @@
 # https://www.microchip.com/en-us/product/pic16f1704
 
 gpdasm -p p16f1704 -csno S19pro-PIC.hex >S19pro-PIC.gpdasm-0.asm
+
+# https://python-intelhex.readthedocs.io/en/latest/part3-1.html
+hex2bin.py S19pro-PIC.hex S19pro-PIC.bin
